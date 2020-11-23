f2145a0bdd208d2cc2234c83025e06fb9b2fb4ce
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-11-23 01:57:15 -0500

    create template for admin
    Implement skeleton page for the Administrator Dashboard.

---
+++ admin-dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <?php require_once('includes/scripts.php');?>
+    <link rel="stylesheet" href="styles/styles.css">
+    <link rel="icon" href="favicon.ico">
+    <title>Admin Dashboard</title>
+</head>
+
+<body>
+</body>
+
+</html>
